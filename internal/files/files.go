c22de1009e0855874b0d5138b73a11bde780d4ae
Nox1KCL <korshunov.maks09@gmail.com>
2026-04-12 14:49:19 +0300

    refactor: remake InDirSorting with the new funcs

--- internal/files/files.go
+++ internal/files/files.go
@@ -141,6 +141,22 @@ func RenameFile(file string) string {
 	return newName
 }
 
+func InDirSorting(scanDir string, cfg *config.Config) (SortResult, error) {
+	sorter := NewSorter(scanDir, cfg)
+	if err := sorter.Scan(); err != nil {
+		return SortResult{}, fmt.Errorf("scanning directory %q: %w", scanDir, err)
+	}
+	if err := sorter.Plan(); err != nil {
+		return SortResult{}, fmt.Errorf("planning sorting: %w", err)
+	}
+
+	if report, err := sorter.Execute(); err != nil {
+		return report, fmt.Errorf("executing sorting: %w", err)
+	} else {
+		return report, nil
+	}
+}
+
 func GetHomeDir() (string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -165,54 +181,4 @@ func GetDownloadsPath() (string, error) {
 	}
 
 	return "", fmt.Errorf("downloads directory not found")
-}
-
-// InDirSorting sorts all files in the target directory according to the provided config
-func InDirSorting(sortPath string, cfg *config.Config) (SortResult, error) {
-	var report SortResult
-
-	entries, err := os.ReadDir(sortPath)
-	if err != nil {
-		return report, fmt.Errorf("reading directory %q: %w", sortPath, err)
-	}
-
-	for _, entry := range entries {
-		fileName := entry.Name()
-		if !entry.IsDir() && !strings.HasPrefix(fileName, ".") {
-			fileExt := filepath.Ext(fileName)
-			targetPath, err := cfg.GetTargetPath(fileExt)
-			if err != nil {
-				report.Skipped = append(report.Skipped, entry.Name())
-				continue
-			}
-
-			var savePath string
-			if filepath.IsAbs(targetPath) {
-				savePath = targetPath
-			} else {
-				savePath = filepath.Join(sortPath, targetPath)
-			}
-
-			// Make the directories by path
-			if err := os.MkdirAll(savePath, 0755); err != nil {
-				report.Errors = append(report.Errors,
-					fmt.Errorf("creating dirs by path %q: %w", savePath, err),
-				)
-				continue
-			}
-
-			oldFilePath := filepath.Join(sortPath, fileName)
-			newFilePath := filepath.Join(savePath, fileName)
-
-			if err := os.Rename(oldFilePath, newFilePath); err != nil {
-				report.Errors = append(report.Errors,
-					fmt.Errorf("moving file from %q to %q: %w", oldFilePath, newFilePath, err),
-				)
-				continue
-			}
-			report.Moved = append(report.Moved, entry.Name())
-		}
-	}
-
-	return report, nil
 }
