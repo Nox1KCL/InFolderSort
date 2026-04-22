9842526bc01b9198d11c7f16d0f84ba3adef8c34
Nox1KCL <korshunov.maks09@gmail.com>
2026-04-22 15:28:58 +0300

    feat(main): initialize and set default structured logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/Nox1KCL/InFolderSort/internal/config"
+	"github.com/Nox1KCL/InFolderSort/internal/logger"
 	"github.com/Nox1KCL/InFolderSort/internal/tui"
 )
 
@@ -19,6 +21,17 @@ func main() {
 		_, _ = fmt.Fprintf(os.Stderr, "get configuration file: %v\n", err)
 		os.Exit(1)
 	}
+
+	levels := map[slog.Level]string{
+		slog.LevelInfo:  "logs/info.log",
+		slog.LevelError: "logs/error.log",
+	}
+	handler, err := logger.GetHandler(&cfg.Logger, levels)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "creating logger: %v\n", err)
+		os.Exit(1)
+	}
+	slog.SetDefault(slog.New(handler))
 
 	// Start tui
 	err = tui.Core(cfg)
