6c1ab97675c50da01f5a189d997cc94fcaca01bb
Nox1KCL <korshunov.maks09@gmail.com>
2026-04-27 23:03:27 +0300

    feat(main): add logging config init, starting tui err

--- cmd/main.go
+++ cmd/main.go
@@ -16,9 +16,9 @@ func main() {
 	flag.Parse()
 	actualConfigPath := *configPath
 
-	cfg, err := config.GetConfig(actualConfigPath)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "get configuration file: %v\n", err)
+	cfg, cfgErr := config.GetConfig(actualConfigPath)
+	if cfgErr != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "get configuration file: %v\n", cfgErr)
 		os.Exit(1)
 	}
 
@@ -26,16 +26,26 @@ func main() {
 		slog.LevelInfo:  "logs/info.log",
 		slog.LevelError: "logs/error.log",
 	}
-	handler, err := logger.GetHandler(&cfg.Logger, levels)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "creating logger: %v\n", err)
+	handler, logErr := logger.GetHandler(&cfg.Logger, levels)
+	if logErr != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "creating logger: %v\n", logErr)
 		os.Exit(1)
 	}
 	slog.SetDefault(slog.New(handler))
+	mlog := slog.With("module", "main")
+
+	mlog.Info("configuration initialized",
+		"config_path", actualConfigPath,
+		"rules_count", len(cfg.Rules),
+	)
 
 	// Start tui
-	err = tui.Core(cfg)
+	err := tui.Core(cfg)
 	if err != nil {
+		mlog.Error("starting tui",
+			"error", err,
+			"config_rules", len(cfg.Rules),
+		)
 		_, _ = fmt.Fprintf(os.Stderr, "running application: %v\n", err)
 		os.Exit(1)
 	}
